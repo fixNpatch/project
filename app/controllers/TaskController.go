f63dba3e2918c630ab4a14c4b82c7702da56b1d9
fixN <seo@gm.com>
2018-09-21 12:04:47 +0400

    Init

--- app/controllers/TaskController.go
+++ app/controllers/TaskController.go
@@ -17,40 +17,6 @@ func (c TaskController) GetUser(id int) revel.Result {
 	return c.RenderJSON(a)
 }
 
-type Task struct {
-	task_number    int
-	project_title  string
-	task_title     string
-	task_hours     int
-	task_timestamp string
-	task_status    string
-	task_doer      User
-}
-
-type User struct {
-	LOGIN              string
-	PASSWORD           string
-	employee_id        int
-	employee_first     string
-	employee_second    string
-	employee_middle    string
-	employee_rank      string
-	employee_pic       string
-	employee_timestamp string
-	taskstack          []Task
-}
-
-type Project struct {
-	project_id          int
-	project_number      int
-	project_title       string
-	project_description string
-	project_status      string
-	project_timestamp   string
-	project_doers       []User
-	taskstack           []Task
-}
-
 func (c TaskController) GetTaskList() revel.Result {
 	file, err := ioutil.ReadFile("C:/Users/dev/go/src/testapp/app/providers/data.json")
 	if err != nil {
---
+++ app/providers/MainModel.go
@@ -0,0 +1,35 @@
+package providers
+
+type Task struct {
+	task_number    int
+	project_title  string
+	task_title     string
+	task_hours     int
+	task_timestamp string
+	task_status    string
+	task_doer      User
+}
+
+type User struct {
+	LOGIN              string
+	PASSWORD           string
+	employee_id        int
+	employee_first     string
+	employee_second    string
+	employee_middle    string
+	employee_rank      string
+	employee_pic       string
+	employee_timestamp string
+	taskstack          []Task
+}
+
+type Project struct {
+	project_id          int
+	project_number      int
+	project_title       string
+	project_description string
+	project_status      string
+	project_timestamp   string
+	project_doers       []User
+	taskstack           []Task
+}
