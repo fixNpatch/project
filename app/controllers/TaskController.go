9baaeed4ff08364ded6a1b6eb761e6d736b69b7c
fixN <seo@gm.com>
2018-09-21 12:29:43 +0400

    Init

--- app/controllers/TaskController.go
+++ app/controllers/TaskController.go
@@ -1,12 +1,47 @@
 package controllers
 
 import (
+	"encoding/json"
 	"github.com/revel/revel"
 	"io/ioutil"
 )
 
 type TaskController struct {
 	*revel.Controller
+}
+
+type Task struct {
+	task_number    int
+	project_title  string
+	task_title     string
+	task_hours     int
+	task_timestamp string
+	task_status    string
+	task_doer      User
+}
+
+type User struct {
+	LOGIN              string
+	PASSWORD           string
+	employee_id        int
+	employee_first     string
+	employee_second    string
+	employee_middle    string
+	employee_rank      string
+	employee_pic       string
+	employee_timestamp string
+	taskstack          []Task
+}
+
+type Project struct {
+	project_id          int
+	project_number      int
+	project_title       string
+	project_description string
+	project_status      string
+	project_timestamp   string
+	project_doers       []User
+	taskstack           []Task
 }
 
 func (c TaskController) Index() revel.Result {
@@ -22,5 +57,10 @@ func (c TaskController) GetTaskList() revel.Result {
 	if err != nil {
 		panic(err)
 	}
-	return c.RenderText(string(file))
+
+	var projects []Project
+
+	json.Unmarshal(file, &projects)
+
+	return c.RenderText(projects[0].project_title)
 }
